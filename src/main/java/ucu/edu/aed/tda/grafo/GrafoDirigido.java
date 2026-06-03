c081a008612a2c5fbbe6c035df07a0b8a8975bba
Gonzalo Di Cono <gonzalo.dicono@correo.ucu.edu.uy>
2026-06-03 19:31:44 -0300

    Division de clases

--- src/main/java/ucu/edu/aed/tda/grafo/GrafoDirigido.java
+++ src/main/java/ucu/edu/aed/tda/grafo/GrafoDirigido.java
@@ -16,7 +16,7 @@ import ucu.edu.aed.tda.grafo.model.result.IDijkstraResult;
 import ucu.edu.aed.tda.grafo.model.result.IFloydWarshallResult;
 import ucu.edu.aed.tda.grafo.model.result.Path;
 
-public class GrafoDirigido<V, D> implements IDirectedIGraph<V, D>, IDirectedGraphAlgorithms{
+public class GrafoDirigido<V, D> implements IDirectedIGraph<V, D>{
 
     public GrafoDirigido(){
         this.adyacencias = new HashMap<>();
@@ -241,50 +241,5 @@ public class GrafoDirigido<V, D> implements IDirectedIGraph<V, D>, IDirectedGrap
         }
         caminoActual.remove(v);
         return false;
-    }
-
-    @Override
-    public <V, D extends WeightedEdge> IDijkstraResult<V> dijkstra(Comparable<V> source, IDirectedIGraph<V, D> grafo) {
-        return null;
-    }
-
-    @Override
-    public <V, D extends WeightedEdge> IFloydWarshallResult<V> floyd(IDirectedIGraph<V, D> grafo) {
-        return null;
-    }
-
-    @Override
-    public <V, D extends WeightedEdge> IFloydWarshallResult<V> warshall(IDirectedIGraph<V, D> grafo) {
-        return null;
-    }
-
-    @Override
-    public <V, D extends WeightedEdge> V obtenerCentroGrafo(IDirectedIGraph<V, D> grafo) {
-        return null;
-    }
-
-    @Override
-    public <V, D extends WeightedEdge> double obtenerExcentricidad(IDirectedIGraph<V, D> grafo, Comparable<V> vertexCriteria) {
-        return 0;
-    }
-
-    @Override
-    public <V, D extends WeightedEdge> List<Path<V>> obtenerTodosLosCaminos(Comparable<V> source, Comparable<V> target, IGraph<V, D> grafo) {
-        return List.of();
-    }
-
-    @Override
-    public <V, D> void recorridoEnProfundidad(IGraph<V, D> grafo, Comparable<V> sourceCriteria, Consumer<V> consumer) {
-
-    }
-
-    @Override
-    public <V, D> void recorridoEnAmplitud(IGraph<V, D> grafo, Comparable<V> sourceCriteria, Consumer<V> consumer) {
-
-    }
-
-    @Override
-    public <V, D> List<V> calcularClasificacionTopologica(IDirectedIGraph<V, D> grafo) {
-        return List.of();
     }
 }
---
+++ src/main/java/ucu/edu/aed/tda/grafo/GrafoDirigidoAlgortimos.java
@@ -0,0 +1,57 @@
+package ucu.edu.aed.tda.grafo;
+
+import ucu.edu.aed.tda.grafo.model.IGraph;
+import ucu.edu.aed.tda.grafo.model.edge.WeightedEdge;
+import ucu.edu.aed.tda.grafo.model.result.IDijkstraResult;
+import ucu.edu.aed.tda.grafo.model.result.IFloydWarshallResult;
+import ucu.edu.aed.tda.grafo.model.result.Path;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class GrafoDirigidoAlgortimos implements IDirectedGraphAlgorithms{
+    @Override
+    public <V, D extends WeightedEdge> IDijkstraResult<V> dijkstra(Comparable<V> source, IDirectedIGraph<V, D> grafo) {
+        return null;
+    }
+
+    @Override
+    public <V, D extends WeightedEdge> IFloydWarshallResult<V> floyd(IDirectedIGraph<V, D> grafo) {
+        return null;
+    }
+
+    @Override
+    public <V, D extends WeightedEdge> IFloydWarshallResult<V> warshall(IDirectedIGraph<V, D> grafo) {
+        return null;
+    }
+
+    @Override
+    public <V, D extends WeightedEdge> V obtenerCentroGrafo(IDirectedIGraph<V, D> grafo) {
+        return null;
+    }
+
+    @Override
+    public <V, D extends WeightedEdge> double obtenerExcentricidad(IDirectedIGraph<V, D> grafo, Comparable<V> vertexCriteria) {
+        return 0;
+    }
+
+    @Override
+    public <V, D extends WeightedEdge> List<Path<V>> obtenerTodosLosCaminos(Comparable<V> source, Comparable<V> target, IGraph<V, D> grafo) {
+        return List.of();
+    }
+
+    @Override
+    public <V, D> void recorridoEnProfundidad(IGraph<V, D> grafo, Comparable<V> sourceCriteria, Consumer<V> consumer) {
+
+    }
+
+    @Override
+    public <V, D> void recorridoEnAmplitud(IGraph<V, D> grafo, Comparable<V> sourceCriteria, Consumer<V> consumer) {
+
+    }
+
+    @Override
+    public <V, D> List<V> calcularClasificacionTopologica(IDirectedIGraph<V, D> grafo) {
+        return List.of();
+    }
+}
