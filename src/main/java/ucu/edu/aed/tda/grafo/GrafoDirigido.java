657edd3c248c2980a7d75132c3674a6da336234f
Gonzalo Di Cono <gonzalo.dicono@correo.ucu.edu.uy>
2026-06-02 22:58:11 -0300

    Creacion de clase GrafodDirigido, implementacion de agregarvertice, buscarvertice, agregararista, eliminararista

---
+++ src/main/java/ucu/edu/aed/tda/grafo/GrafoDirigido.java
@@ -0,0 +1,146 @@
+package ucu.edu.aed.tda.grafo;
+
+import ucu.edu.aed.tda.grafo.model.edge.DirectedEdge;
+import ucu.edu.aed.tda.grafo.model.edge.Edge;
+
+import java.util.*;
+
+public class GrafoDirigido<V, D> implements IDirectedIGraph<V, D>{
+
+    public GrafoDirigido(){
+        this.adyacencias = new HashMap<>();
+    }
+
+    private final Map<V, Set<Edge<V, D>>> adyacencias;
+
+    @Override
+    public Set<V> successors(Comparable<V> criteria) {
+        return Set.of();
+    }
+
+    @Override
+    public Set<V> predecessors(Comparable<V> criteria) {
+        return Set.of();
+    }
+
+    @Override
+    public boolean agregarVertice(V vertex) {
+        if (vertex == null) {
+            return false;
+        }
+        if (adyacencias.containsKey(vertex)) {
+            return false;
+        }
+        adyacencias.put(vertex, new HashSet<>()); // iniciamos el conjunto de aristas para el nuevo vertice
+        return true; // deveuvle true si el vertice se agregó
+    }
+
+    @Override
+    public V buscarVertice(Comparable<V> criterio) {
+       for (V v : adyacencias.keySet()) { // recorre todos los vertices del mapa de adyacencias
+           if (criterio.compareTo(v) == 0) {
+               return v; // retorna el vertice si coincide segun el criterio
+           }
+       }
+       return null;
+    }
+
+    @Override
+    public boolean agregarArista(V source, V target, D dato) {
+
+        // verifica la existencia de amsbos vertices en el grafo
+        if (!adyacencias.containsKey(source)) {
+            return false;
+        }
+
+        if (!adyacencias.containsKey(target)) {
+            return false;
+        }
+
+        // creamos la nueva arista con sentido
+        Edge <V,D> aristaNueva = new DirectedEdge<>(source, target, dato);
+
+        // conjunto de aristas que parten del vertice de origen
+        Set<Edge<V, D>> conexionesSource = adyacencias.get(source);
+
+        if (conexionesSource.add(aristaNueva)) { // .add devuvelve false si el elemtno ya exise en el conjutno
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean eliminarArista(Comparable<V> source, Comparable<V> target) {
+
+        // intentamos encontrar el vertice de origen usando el criterio
+      V origen = buscarVertice(source);
+      if (origen == null) {
+          return false;
+      }
+
+      // Obtiene el conjutno de aristas del vertice de origen
+      Set<Edge<V, D>> aristas = adyacencias.get(origen);
+      boolean eliminado = false;
+
+      // usamos iterator para poder remover elemntos de forma segura mientras recorremos la coleccion
+      Iterator<Edge<V, D>> iterator = aristas.iterator();
+      while (iterator.hasNext()) {
+          Edge<V, D> arista = iterator.next();
+
+          // si el destino de la arista actual coincide con el criterio buscado, esta se elimina
+          if(target.compareTo(arista.target()) == 0)
+          {
+              iterator.remove(); // elimina a travez del iterator (segura)
+              eliminado = true; // nos dice que al menos un elemento fue elikmiando
+
+          }
+      }
+      return eliminado;
+    }
+
+    @Override
+    public boolean removerVertice(Comparable<V> criteria) {
+        return false;
+    }
+
+    @Override
+    public Set<V> vertices() {
+        return Set.of();
+    }
+
+    @Override
+    public Set<Edge<V, D>> aristas() {
+        return Set.of();
+    }
+
+    @Override
+    public boolean existeArista(Comparable<V> sourceCriteria, Comparable<V> targetCriteria) {
+        return false;
+    }
+
+    @Override
+    public Edge<V, D> obtenerArista(Comparable<V> sourceCriteria, Comparable<V> targetCriteria) {
+        return null;
+    }
+
+    @Override
+    public List<Edge<V, D>> adyacencias(Comparable<V> verticeCriteria) {
+        return List.of();
+    }
+
+    @Override
+    public boolean esConexo() {
+        return false;
+    }
+
+    @Override
+    public void vaciar() {
+
+    }
+
+    @Override
+    public boolean tieneCiclos() {
+        return false;
+    }
+}
